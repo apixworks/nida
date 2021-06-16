86666247b66fa35852a283e92e122192bf8f9403
apixworks <hewlettpin777@gmail.com>
2021-06-16 14:10:08 +0300

    Initial commit

--- lib/infrastracture/nida_repository.dart
+++ lib/infrastracture/nida_repository.dart
@@ -1,5 +1,16 @@
 import 'package:dartz/dartz.dart';
+import 'package:nida/domain/nida.dart';
 
 abstract class INidaRepository{
   Future<Either<String, Nida>> getNidaInfo({int Nin});
+}
+
+
+class NidaRepository implements INidaRepository{
+  @override
+  Future<Either<String, Nida>> getNidaInfo({int Nin}) {
+    // TODO: implement getNidaInfo
+    throw UnimplementedError();
+  }
+
 }
