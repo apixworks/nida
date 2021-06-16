229b29338e32ca34a9f5714ab462b68efd55808b
apixworks <hewlettpin777@gmail.com>
2021-06-16 12:45:13 +0300

    Initial commit

--- lib/infrastracture/nida_dto.dart
+++ lib/infrastracture/nida_dto.dart
@@ -1,18 +1,22 @@
-import 'package:flutter/foundation.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'nida_dto.freezed.dart';
-part 'nida_dto.g.dart';
-
-@freezed
-abstract class  with _$ {
-  @JsonSerializable(fieldRename: FieldRename.snake, explicitToJson: true)
-  const factory ({
-    ,
-  }) = _;
-
-
-
-  factory .fromJson(Map<String, dynamic> json) =>
-      _$FromJson(json);
-}
+// import 'package:flutter/foundation.dart';
+// import 'package:freezed_annotation/freezed_annotation.dart';
+//
+// part 'nida_dto.freezed.dart';
+// part 'nida_dto.g.dart';
+//
+// @freezed
+// abstract class NidaDto with _$NidaDto {
+//   @JsonSerializable(fieldRename: FieldRename.snake, explicitToJson: true)
+//   const factory NidaDto({
+//     String FirstName,
+//     String MiddleName,
+//     String LastName,
+//     String Sex,
+//     String DateofBirth
+//   }) = _NidaDto;
+//
+//
+//
+//   factory NidaDto.fromJson(Map<String, dynamic> json) =>
+//       _$NidaDtoFromJson(json);
+// }
